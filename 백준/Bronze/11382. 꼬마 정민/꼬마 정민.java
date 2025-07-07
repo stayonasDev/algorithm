c3e1efe2079198b9cca16d649aca1597e4b4879e
김윤환 <149605122+stayonasDev@users.noreply.github.com>
2025-07-07 15:40:27 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        Long A = in.nextLong();
+        Long B = in.nextLong();
+        Long C = in.nextLong();        
+        System.out.println(A + B + C);
+    }
+}
