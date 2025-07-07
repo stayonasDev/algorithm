04baf1f1c2744a08d56b242d2d72dbcb70e815ff
김윤환 <149605122+stayonasDev@users.noreply.github.com>
2025-07-07 15:25:36 +0900

    [level 2] Title: H-Index, Time: 0.90 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        for(int h = citations.length; h >= 1; h--){
+            if(isValid(citations, h)) return h;
+        }
+        return 0;
+    }
+    
+    private boolean isValid(int[] citations, int h){
+        int index = citations.length - h;
+        return citations[index] >= h;
+    }
+}
