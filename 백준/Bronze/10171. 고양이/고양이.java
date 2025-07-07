f2efce74e9408f5dbd5b5a802329fd966e7fd319
김윤환 <149605122+stayonasDev@users.noreply.github.com>
2025-07-07 15:46:37 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		String[] cat1 = { "\\    /\\", " )  ( ')", "(  /  )", " \\(__)|" };
+		for (String p : cat1)
+			System.out.println(p);
+	}
+}
