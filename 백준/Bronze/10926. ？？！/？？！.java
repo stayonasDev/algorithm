08722d46bd134c28ecc3b8b42cae6655045da93f
김윤환 <149605122+stayonasDev@users.noreply.github.com>
2025-07-07 15:43:15 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        
+        name += "??!";
+        System.out.println(name);
+    }
+}
