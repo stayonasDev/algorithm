7e22daa17db40ccd567ae54a048c6fbd4fbdccd7
김윤환 <149605122+stayonasDev@users.noreply.github.com>
2025-07-07 15:46:27 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		String[] cat1 = { "|\\_/|", "|q p|   /}", "( 0 )\"\"\"\\", "|\"^\"`    |", "||_/=\\\\__|" };
+		for (String p : cat1)
+			System.out.println(p);
+	}
+}
