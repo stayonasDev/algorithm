ac05b2750b2b075f1c736cc37c28cf4a5d59cd9b
yoon <stay_on_track@naver.com>
2025-07-08 15:57:36 +0900

    문자열 수 세기

---
+++ 학습/자바코딩문제집/001_문자 수 세기/문자열 개수 세기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+
+        for (String token : str.split(" ")) {
+            sb.append(token);
+        }
+        System.out.println(sb.length());
+    }
+}
