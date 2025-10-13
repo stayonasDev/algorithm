0746bda41cf8312747dcc601b0d736b132c43680
김윤환 <149605122+stayonasDev@users.noreply.github.com>
2025-10-13 13:57:55 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.30 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,14 +1,13 @@
 import java.util.Arrays;
-import java.util.Collections;
 
-class Solution
-{
+class Solution{
+    //코드 설명: 제일 작은 값을 구하려면 제일 작은 값을 제일 큰 값과 곱해야 함
+    //Sort로 정렬된 A를 정렬된 B의 역순으로 곱함, Big O(N)으로 만들기 위해 For 하나로 
     public int solution(int []A, int []B){
         int result = 0;
         Arrays.sort(A);
         Arrays.sort(B);
 
-        
         for(int i = B.length - 1; i >= 0; i--)
             result += A[B.length - 1 - i] * B[i];
         
